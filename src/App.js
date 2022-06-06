46e4b1bcdaf5b131db251a75e40c07313595f21c
Tushar <tushar.nandy@vidyamantra.com>
2022-06-06 17:27:11 +0530

    proper route page added for profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Switch, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
-        <Route path='/profile' element={<UserProfile />} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </Layout>
   );
