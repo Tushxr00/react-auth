ce4f1e30ae88c23d5ce979d61ebaca930b34c9d0
Tushar <tushar.nandy@vidyamantra.com>
2022-06-10 09:47:11 +0530

    router guard applied

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
+import AuthContext from "./store/auth- context";
 
 function App() {
+  const authCtx = useContext(AuthContext)
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth" element={<AuthPage />} />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/profile" element={<ProfilePage />} />
+        )}
+        <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
     </Layout>
   );
